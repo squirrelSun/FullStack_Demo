2b6489546cea7019a8be5602363aba4e9e0ea5cb
squirrelSun <50691703+squirrelSun@users.noreply.github.com>
2019-12-07 20:16:29 +0800

    集合练习

---
+++ Collection/Map_test.java
@@ -0,0 +1,13 @@
+package Collection;
+/*
+ * 集合框架
+ *
+ * Java容器：数组、集合等对多个数据进行存储（内存层面）操作的结构
+ * 
+ * Map接口：双列数据，保存具有映射关系“key-value对”的集合
+ *		HashMap、LinkedHashMap、TreeMap、HashTable、Properties
+ *
+ * */
+public class Map_test {
+
+}
